3385dfe5f323df2639d6f64cf4001f7312b111f4
schamma94 <scha94@hotmail.com>
2024-11-19 16:59:29 +1100

    Added Open Source link and name below

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import "./Weather.css";
+import "./Search.css";
 import Weather from "./Weather";
 
 function App() {
@@ -6,6 +8,17 @@ function App() {
     <div className="App">
       <div className="container border border-4 shadow">
         <Weather />
+      </div>
+      <div className="open-source">
+        <a
+          className="source-link"
+          href="https://github.com/schamma94/sarahc-weather-react"
+          target="_blank"
+        >
+          {" "}
+          Open Source Code
+        </a>{" "}
+        by Sarah Chamma
       </div>
     </div>
   );
