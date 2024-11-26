9d3a49eb270c49ca7bb4850cb6f6324928c02f74
schamma94 <scha94@hotmail.com>
2024-11-26 15:04:52 +1100

    Cleanup of templates and added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <div className="container border border-4 shadow">
-        <Weather />
-      </div>
-      <div className="open-source">
-        <a
-          className="source-link"
-          href="https://github.com/schamma94/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Open Source Code
-        </a>{" "}
-        by Sarah Chamma
+      <div className="container">
+        <div className="container border border-4 shadow">
+          <Weather />
+        </div>
+        <div className="open-source">
+          <a
+            className="source-link"
+            href="https://github.com/schamma94/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Open Source Code
+          </a>{" "}
+          by Sarah Chamma
+        </div>
       </div>
     </div>
   );
