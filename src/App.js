4c7c0df2235a6c4b5863020b11ea2b34ea2e7831
schamma94 <scha94@hotmail.com>
2024-12-06 18:30:25 +1100

    Added Gif

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import "./Weather.css";
 import "./Search.css";
+import pusheenGif from "./pusheen-gif.gif";
 import Weather from "./Weather";
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
     <div className="App">
       <div className="container border border-4 shadow">
         <Weather defaultCity="Melbourne" />
+      </div>
+      <div className="gif-container">
+        <img className="pusheen-gif" src={pusheenGif} alt="Pusheen GIF" />
       </div>
       <div className="open-source">
         <a
